202a9afef3a6d075b7198a32b6a34189414033b3
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-12 14:33:41 +0800

    setup(project): setup database and create react app

---
+++ springreact/src/main/java/com/example/springreact/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.springreact.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springreact.entity.User;
+import com.example.springreact.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+}
