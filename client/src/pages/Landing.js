300b9f7328ad032a37e592cef405d6a3c8ef037d
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-28 11:15:26 +0800

    setup(project): add apis and pages

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,9 @@
+import Grid from "@mui/material/Grid";
+
+export default function Landing() {
+    return (
+        <Grid container sx={{ display: "flex", border: "1px solid black", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <h1>Welcome dumbass!</h1>
+        </Grid>
+    );
+}
--- springreact/src/main/java/com/example/springreact/repository/UserRepository.java
+++ springreact/src/main/java/com/example/springreact/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.springreact.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,6 @@ import com.example.springreact.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findByEmail(String email);
-    
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
